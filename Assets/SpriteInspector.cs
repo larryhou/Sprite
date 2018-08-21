6d497edc6e3eed4c8c6f636b556bf2981d4c2ad7
larryhou <larryhou@larryhou-rmbp.local>
2018-08-21 13:38:23 +0800

    update

--- Assets/SpriteInspector.cs
+++ Assets/SpriteInspector.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
-using System;
 
 public static class SpriteExtension
 {
